ae968e1146da4d35102eb455f670a33d40efa158
Abode1919 <ghosenabdo7@gmail.com>
2024-09-24 19:01:25 +0200

    Update Tabeller.java

--- src/main/java/no/hvl/dat100/tabeller/Tabeller.java
+++ src/main/java/no/hvl/dat100/tabeller/Tabeller.java
@@ -33,12 +33,12 @@ public class Tabeller {
 	public static int summer(int[] tabell) {
 		int sum = 0;
 	
-		// Iterer gjennom alle elementene i tabellen og legg dem til summen
+		
 		for (int i = 0; i < tabell.length; i++) {
 			sum += tabell[i];
 		}
 	
-		// Returner den totale summen
+		
 		return sum;
 	}
 
@@ -46,56 +46,54 @@ public class Tabeller {
 	public static boolean finnesTall(int[] tabell, int tall) {
 		for (int i = 0; i < tabell.length; i++) {
 			if (tabell[i] == tall) {
-				return true; // Returner true hvis tallet finnes
+				return true; 
 			}
 		}
-		return false; // Returner false hvis tallet ikke finnes
+		return false; 
 	}
 
 	// e)
 	public static int posisjonTall(int[] tabell, int tall) {
 		for (int i = 0; i < tabell.length; i++) {
 			if (tabell[i] == tall) {
-				return i; // Returner indeksen til det første forekomsten
+				return i; 
 			}
 		}
-		return -1; // Returner -1 hvis tallet ikke finnes
+		return -1; 
 	}
 
 	// f)
 	public static int[] reverser(int[] tabell) {
-		int[] nyTabell = new int[tabell.length]; // Ny tabell med samme lengde
+		int[] nyTabell = new int[tabell.length]; 
 		for (int i = 0; i < tabell.length; i++) {
-			nyTabell[i] = tabell[tabell.length - 1 - i]; // Sett inn elementene i motsatt rekkefølge
+			nyTabell[i] = tabell[tabell.length - 1 - i]; 
 		}
-		return nyTabell; // Returner den nye reverserte tabellen
+		return nyTabell; 
 	}
 
 	// g)
 	public static boolean erSortert(int[] tabell) {
 		for (int i = 1; i < tabell.length; i++) {
 			if (tabell[i] < tabell[i - 1]) {
-				return false; // Returner false hvis et element er mindre enn det forrige
+				return false; 
 			}
 		}
-		return true; // Returner true hvis tabellen er sortert
+		return true; 
 	}
 
 	// h)
 	public static int[] settSammen(int[] tabell1, int[] tabell2) {
-		int[] nyTabell = new int[tabell1.length + tabell2.length]; // Ny tabell med summen av lengdene
+		int[] nyTabell = new int[tabell1.length + tabell2.length]; 
 		int index = 0;
 	
-		// Legg til elementene fra tabell1
 		for (int i = 0; i < tabell1.length; i++) {
 			nyTabell[index++] = tabell1[i];
 		}
 	
-		// Legg til elementene fra tabell2
 		for (int i = 0; i < tabell2.length; i++) {
 			nyTabell[index++] = tabell2[i];
 		}
 	
-		return nyTabell; // Returner den sammensatte tabellen
+		return nyTabell; 
 	}
 }
